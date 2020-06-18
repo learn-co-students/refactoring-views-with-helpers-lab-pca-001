eba3dd36cd34290c56dfc309919e55325c63fe86
Andrew Kloecker <lt.kvothe@gmail.com>
2020-06-18 09:26:50 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name
   end
 
   def artist_name=(name)
+    tmp = Artist.find_or_create_by(name: name)
+    tmp.songs << self
+    tmp.save
   end
 end
